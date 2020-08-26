de67c56b0ffdac96c2b0a012767e8d7f21106ce5
n00bmasterr <55821961+n00bmasterr@users.noreply.github.com>
2020-08-26 18:55:16 +1000

    Siddhesh Kale branch (#2)
    * Added files to test the registration form
    
    * Added more files

--- quizaris.py
+++ quizaris.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+# Import form from the forms.py file
+from forms import RegistrationForm, LoginForm
 # Creating an instance of flask
 app = Flask(__name__)
 
+# Set a cookie to avoid CSRF attacks
+app.config['SECRET_KEY'] = '23ac083317c8fd13abc6e424e9425b4f'
+
 posts = [
 	{
-		'author': 'Test',
-		'title': 'testing',
+		'author': 'Me',
+		'title': 'KAKAKAK',
 		'content': 'First question',
 		'date_posted': 'Today'
+	}, 
+	{
+		'author': 'You',
+		'title': 'NayNay',
+		'content': 'Second Questions',
+		'date_posted': 'last nmight'
 	}
 ]
 
@@ -20,3 +31,12 @@ def hello():
 def team():
 	return render_template("test.html", posts=posts)
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	return render_template('test-form.html', title="Test", form=form)
+
+# @app.route("/login")
+# def register():
+# 	form = LoginForm()
+# 	return render_template('test-form.html', title="Test", form=form)
