aa1b31ddaa3c43b9ec10a8efc26eed52a8b445a8
n00bmasterr <55821961+n00bmasterr@users.noreply.github.com>
2020-09-08 21:56:28 +1000

    Merge the last 2 commit with additional table and requirements.txt file (#14)
    * Made a few changes to the files and added a new table for the databse
    
    * Added a requirements.txt file for easier dependency downloads

--- run_app.py
+++ run_app.py
@@ -4,4 +4,4 @@ from quizaris import app
 # The value of __name__ returns '__main__', __name__ is a special variable which sets the variable value of 
 # __name__ to __main__ if that file is the main file e.g. python3 quizaris.py will return __main__. 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=80)
