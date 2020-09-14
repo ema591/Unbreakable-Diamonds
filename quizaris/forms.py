101e8afbfc299dfebce2647fba977c0c8ed8bec3
n00bmasterr <55821961+n00bmasterr@users.noreply.github.com>
2020-09-14 19:58:10 +1000

    Siddhesh kale branch (#16)
    * Made a few changes to the files and added a new table for the databse
    
    * Added a requirements.txt file for easier dependency downloads
    
    * removcing pycache
    
    * Added a bunch forms and routes for quizzes

--- quizaris/forms.py
+++ quizaris/forms.py
@@ -6,7 +6,7 @@
 # Flask forms will be used to make forms for submissions
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, DecimalField, SelectField, RadioField
 from quizaris.database import User
 
 class RegistrationForm(FlaskForm):
@@ -19,7 +19,7 @@ class RegistrationForm(FlaskForm):
 	# The following set of validation functions will check whether the user data exists in order to avoid conflicts in the database
 	def validate_username(self, username):
 		# If the username exists this user = True if not it will be None 
-		user  = User.query.filter_by(username=username.data).first()
+		user = User.query.filter_by(username=username.data).first()
 		if user:
 			# Validation error is built in to the wtforms.validators library and will be returned as a form error.
 			raise ValidationError('The username already exists! Please pick a different username!')
@@ -34,3 +34,24 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Login')
+
+class AddQuestion(FlaskForm):
+	question = StringField('Question', validators=[DataRequired()])
+	option_a = StringField('Option A', validators=[DataRequired()])
+	option_b = StringField('Option B', validators=[DataRequired()])
+	option_c = StringField('Option C')
+	option_d = StringField('Option D')
+	answer = DecimalField('Answer (1, 2, 3, or 4)', validators=[DataRequired()])
+	category = SelectField('Category', choices=[('science', 'Science'), ('maths', 'Maths'), ('english', 'English')])
+	difficulty = SelectField('Difficulty', choices=[('easy', 'Easy'), ('intermediate', 'Intermediate'), ('hard', 'Hard')])
+	submit = SubmitField('Submit Question')
+
+# The following form will be used by the users to choose the type of quiz that they want to do.
+class ChooseTypeQuiz(FlaskForm):
+	category = SelectField('Category', choices=[('science', 'Science'), ('maths', 'Maths'), ('english', 'English')])
+	difficulty = SelectField('Difficulty', choices=[('easy', 'Easy'), ('intermediate', 'Intermediate'), ('hard', 'Hard')])
+	submit = SubmitField('Choose quiz')
+
+class SolveQuiz(FlaskForm, option_a, option_b, option_c, option_d):
+	options = RadioField('Options', choices=[('1', option_a), ('2', option_b), ('3', option_c), ('4', option_d)])
+	submit = SubmitField('Submit Answer')
